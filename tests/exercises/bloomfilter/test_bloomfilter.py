a0d2b8291c007a32d9898d6a87e463f61c81a653
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-06-08 22:55:50 -0500

    Update tests

--- tests/exercises/bloomfilter/test_bloomfilter.py
+++ tests/exercises/bloomfilter/test_bloomfilter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-# encoding: UTF-8
 """
+Testing the Bloom filter
 @author: Roman Yasinovskyy
-@date: 2019
-@module: project.bloomfilter
+@date: 2020
 """
 
 import pathlib
@@ -11,48 +10,79 @@ import pytest
 import toml
 from src.exercises.bloomfilter import BloomFilter
 
-THIS_DIR = pathlib.Path(".")
-TEST_DIR = pathlib.Path("tests/exercises/bloomfilter/")
-DATA_DIR = pathlib.Path("data/exercises/bloomfilter/")
-FILE_PUBLIC = pathlib.Path("test_bloomfilter_public.toml")
-FILE_SECRET = pathlib.Path("test_bloomfilter_secret.toml")
+TIME_LIMIT = 2
 
-if "tests" not in THIS_DIR.parts:
-    FILE_PUBLIC = THIS_DIR / TEST_DIR / FILE_PUBLIC
-    FILE_SECRET = THIS_DIR / TEST_DIR / FILE_SECRET
 
-all_test_cases = toml.load(FILE_PUBLIC)
+def get_cases(category: str):
+    with open(pathlib.Path(__file__).with_suffix(".toml")) as f:
+        all_cases = toml.load(f)
+        for case in all_cases[category]:
+            yield (case.get("dictionary"), case.get("typos"), case.get("expected"))
 
-TIME_LIMIT = 1
-WORDS = [
-    (v.get("data")[0], v.get("data")[1], v.get("expected"))
-    for v in all_test_cases.get("case_public").get("success").values()
-]
 
-if FILE_SECRET.exists():
-    all_test_cases.update(toml.load(FILE_SECRET))
-    WORDS.extend(
-        [
-            (v.get("data")[0], v.get("data")[1], v.get("expected"))
-            for v in all_test_cases.get("case_secret").get("success").values()
-        ]
-    )
+@pytest.fixture(scope="session", autouse=True)
+def bf():
+    """Build Bloom filter"""
+    my_filter = BloomFilter(980600, 7)
+    with open("data/exercises/bloomfilter/words") as file_in:
+        for word in file_in.readlines():
+            my_filter.add(word.strip())
+    return my_filter
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("dictionary, typos, expected", WORDS)
-def test_bloomfilter(dictionary, typos, expected):
+@pytest.mark.parametrize("dictionary, typos, expected", get_cases("small_dictionary"))
+def test_bloomfilter_1(dictionary, typos, expected):
     """Testing the output"""
-    bf = BloomFilter(250, 7)
-    with open(DATA_DIR / pathlib.Path(dictionary)) as file_in:
+    bf = BloomFilter(100, 7)
+    with open(dictionary) as file_in:
         for word in file_in.readlines():
             bf.add(word.strip())
     count = 0
-    with open(DATA_DIR / pathlib.Path(typos)) as file_in:
+    with open(typos) as file_in:
         for word in file_in.readlines():
             if word.strip() in bf:
                 count += 1
-    assert count <= expected
+    assert count == expected
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("dictionary, typos, expected", get_cases("small_dictionary"))
+def test_bloomfilter_2(dictionary, typos, expected):
+    """Testing the output"""
+    bf = BloomFilter(250, 7)
+    with open(dictionary) as file_in:
+        for word in file_in.readlines():
+            bf.add(word.strip())
+    count = 0
+    with open(typos) as file_in:
+        for word in file_in.readlines():
+            if word.strip() in bf:
+                count += 1
+    assert count == 0
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("dictionary, typos, expected", get_cases("large_dictionary"))
+def test_bloomfilter_3(bf, dictionary, typos, expected):
+    """Testing the output"""
+    count = 0
+    with open(typos) as file_in:
+        for word in file_in.readlines():
+            if word.strip() in bf:
+                count += 1
+    assert count == expected
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+def test_bloomfilter_len():
+    """Testing the filter size"""
+    my_filter = BloomFilter(980600, 7)
+    with open("data/exercises/bloomfilter/words") as file_in:
+        for word in file_in.readlines():
+            my_filter.add(word.strip())
+
+    assert len(my_filter) == 980600
 
 
 if __name__ == "__main__":
