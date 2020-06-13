72ed225d47e81c7f18f376fa987a030fd67cb0d3
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-06-13 18:50:12 -0500

    Update tests for classy

--- tests/projects/classy/test_classy.py
+++ tests/projects/classy/test_classy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-# encoding: UTF-8
 """
+Testing the classy project
 @author: Roman Yasinovskyy
-@date: 2019
-@module: project.classy
+@date: 2020
 """
 
 import pathlib
@@ -11,60 +10,52 @@ import pytest
 import toml
 from src.projects.classy import read_file, classify
 
-THIS_DIR = pathlib.Path(".")
-TEST_DIR = pathlib.Path("tests/projects/classy/")
-DATA_DIR = pathlib.Path("data/projects/classy/")
-FILE_PUBLIC = pathlib.Path("test_classy_public.toml")
-FILE_SECRET = pathlib.Path("test_classy_secret.toml")
-
-if "tests" not in THIS_DIR.parts:
-    FILE_PUBLIC = THIS_DIR / TEST_DIR / FILE_PUBLIC
-    FILE_SECRET = THIS_DIR / TEST_DIR / FILE_SECRET
-
-all_test_cases = toml.load(FILE_PUBLIC)
-
 TIME_LIMIT = 1
-PEOPLE = [
-    (v.get("data"), v.get("expected"))
-    for v in all_test_cases.get("case_public").get("success").values()
-]
 
-if FILE_SECRET.exists():
-    all_test_cases.update(toml.load(FILE_SECRET))
-    PEOPLE.extend(
-        [
-            (v.get("data"), v.get("expected"))
-            for v in all_test_cases.get("case_secret").get("success").values()
-        ]
-    )
+
+def get_cases(category: str):
+    with open(pathlib.Path(__file__).with_suffix(".toml")) as f:
+        all_cases = toml.load(f)
+        for case in all_cases[category]:
+            yield (case.get("filename"), case.get("expected"))
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("data, expected", PEOPLE)
-def test_read_file(data, expected):
+@pytest.mark.parametrize("filename, expected", get_cases("test_case"))
+def test_read_file(filename, expected):
     """Testing the file reader"""
-    relatives = read_file(DATA_DIR / pathlib.Path(data))
+    relatives = read_file(filename)
     assert len(relatives) == len(expected)
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_classifier():
+@pytest.mark.parametrize(
+    "zoo, expected",
+    [
+        ({"Ant": "upper", "Bee": "middle", "Cat": "lower"}, ["Ant", "Bee", "Cat"]),
+        ({"Dog": "middle", "Elk": "upper", "Frog": "lower"}, ["Elk", "Dog", "Frog"]),
+        (
+            {
+                "Aardvark": "upper",
+                "Beaver": "upper-upper",
+                "Cheetah": "middle-upper",
+                "Dolphin": "upper",
+                "Elephant": "upper-middle-upper",
+            },
+            ["Beaver", "Elephant", "Aardvark", "Cheetah", "Dolphin"],
+        ),
+    ],
+)
+def test_classifier(zoo, expected):
     """Testing the classifier"""
-    zoo = {
-        "Aardvark": "upper",
-        "Beaver": "upper-upper",
-        "Cheetah": "middle-upper",
-        "Dolphin": "upper",
-        "Elephant": "upper-middle-upper"
-    }
-    assert classify(zoo) == ["Beaver", "Elephant", "Aardvark", "Cheetah", "Dolphin"]
+    assert classify(zoo) == expected
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("data, expected", PEOPLE)
-def test_classy(data, expected):
+@pytest.mark.parametrize("filename, expected", get_cases("test_case"))
+def test_classy(filename, expected):
     """Testing the output"""
-    relatives = read_file(DATA_DIR / pathlib.Path(data))
+    relatives = read_file(filename)
     assert classify(relatives) == expected
 
 
