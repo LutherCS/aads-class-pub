3dfc1c6748d3e3c6bfd5b550f1623adcd665778d
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-01-03 22:29:47 -0600

    Update AVL trees exercise and test cases

--- tests/exercises/avl_tree/test_avl_tree.py
+++ tests/exercises/avl_tree/test_avl_tree.py
@@ -1,105 +1,144 @@
 #!/usr/bin/env python3
 """
-AVL tree test
+Testing the avl tree
+@authors: Roman Yasinovskyy
+@date: 2021
 """
 
-
+import pathlib
 import pytest
+import toml
 from src.exercises.avl_tree import AVLTree
 
 
-class TestBalancedBinarySearchTreeMethods:
-    @pytest.fixture(autouse=True)
-    def setup_class(self):
-        """Setting up"""
-        self.avl_tree = AVLTree()
+TIME_LIMIT = 1
 
-    def test_init(self):
-        """Testing __init__() method"""
-        assert self.avl_tree.root is None
-        self.avl_tree.put(30, "a")
-        assert self.avl_tree.root is not None
 
-    def test_len(self):
-        """Testing __len__() method"""
-        assert self.avl_tree.size() == 0
-        self.avl_tree.put(30, "a")
-        assert len(self.avl_tree) == 1
+def get_cases(category: str):
+    with open(pathlib.Path(__file__).with_suffix(".toml")) as f:
+        all_cases = toml.load(f)
+        for case in all_cases[category]:
+            yield (case.get("nodes"), case.get("traversal"))
 
-    def test_auto_1(self):
-        """Testing case 1: RL rotation"""
-        self.avl_tree.put(30, "a")
-        self.avl_tree.put(50, "b")
-        self.avl_tree.put(40, "c")
-        assert self.avl_tree.root.key == 40
-        assert self.avl_tree.root.balance == 0
-        assert "30 40 50" == " ".join([str(x) for x in self.avl_tree])
 
-    def test_auto_2(self):
-        """Testing case 2: LR rotation"""
-        self.avl_tree.put(50, "a")
-        self.avl_tree.put(30, "b")
-        self.avl_tree.put(40, "c")
-        assert self.avl_tree.root.key == 40
-        assert self.avl_tree.root.balance == 0
-        assert "30 40 50" == " ".join([str(x) for x in self.avl_tree])
+def test_init():
+    """Testing __init__() method"""
+    avl_tree = AVLTree()
+    assert avl_tree.root is None
+    avl_tree.put(30, "a")
+    assert avl_tree.root is not None
 
-    def test_auto_3(self):
-        """Testing case 3: L rotation"""
-        self.avl_tree.put(50, "a")
-        self.avl_tree.put(30, "b")
-        self.avl_tree.put(70, "c")
-        self.avl_tree.put(80, "c")
-        self.avl_tree.put(60, "d")
-        self.avl_tree.put(90, "e")
-        assert self.avl_tree.root.key == 70
-        assert self.avl_tree.root.balance == 0
-        assert "30 50 60 70 80 90" == " ".join([str(x) for x in self.avl_tree])
 
-    def test_auto_4(self):
-        """Testing case 4: R rotation"""
-        self.avl_tree.put(50, "a")
-        self.avl_tree.put(30, "b")
-        self.avl_tree.put(70, "c")
-        self.avl_tree.put(10, "c")
-        self.avl_tree.put(20, "d")
-        self.avl_tree.put(5, "e")
-        assert self.avl_tree.root.key == 20
-        assert self.avl_tree.root.balance == 0
-        assert "5 10 20 30 50 70" == " ".join([str(x) for x in self.avl_tree])
+def test_len():
+    """Testing __len__() method"""
+    avl_tree = AVLTree()
+    assert avl_tree.size() == 0
+    avl_tree.put(30, "a")
+    assert avl_tree.size() == 1
 
-    def test_auto_5(self):
-        """Testing case 4: RL rotation"""
-        self.avl_tree.put(40, "a")
-        self.avl_tree.put(30, "b")
-        self.avl_tree.put(50, "c")
-        self.avl_tree.put(45, "d")
-        self.avl_tree.put(60, "e")
-        self.avl_tree.put(43, "f")
-        assert self.avl_tree.root.key == 45
-        assert self.avl_tree.root.balance == 0
-        assert self.avl_tree.root.child_left.key == 40
-        assert self.avl_tree.root.child_left.balance == 0
-        assert self.avl_tree.root.child_right.key == 50
-        assert self.avl_tree.root.child_right.balance == 1
-        assert "30 40 43 45 50 60" == " ".join([str(x) for x in self.avl_tree])
 
-    def test_auto_6(self):
-        """Testing case 6: LR rotation"""
-        self.avl_tree.put(40, "a")
-        self.avl_tree.put(30, "b")
-        self.avl_tree.put(50, "c")
-        self.avl_tree.put(10, "d")
-        self.avl_tree.put(35, "e")
-        self.avl_tree.put(37, "f")
-        assert self.avl_tree.root.key == 35
-        assert self.avl_tree.root.balance == 0
-        assert self.avl_tree.root.child_left.key == 30
-        assert self.avl_tree.root.child_left.balance == -1
-        assert self.avl_tree.root.child_right.key == 40
-        assert self.avl_tree.root.child_right.balance == 0
-        assert "10 30 35 37 40 50" == " ".join([str(x) for x in self.avl_tree])
+def test_balance():
+    """Testing balance property"""
+    avl_tree = AVLTree()
+    avl_tree.put(30, "a")
+    assert avl_tree.root.balance == 0
+
+
+def test_RL_rotation_simple():
+    """Testing case 1: RL rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(30, "a")
+    avl_tree.put(50, "b")
+    avl_tree.put(40, "c")
+    assert avl_tree.root.key == 40
+    assert avl_tree.root.balance == 0
+    assert " ".join([str(x) for x in avl_tree]) == "30 40 50"
+
+
+def test_LR_rotation_simple():
+    """Testing case 2: LR rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(50, "a")
+    avl_tree.put(30, "b")
+    avl_tree.put(40, "c")
+    assert avl_tree.root.key == 40
+    assert avl_tree.root.balance == 0
+    assert " ".join([str(x) for x in avl_tree]) == "30 40 50"
+
+
+def test_L_rotation():
+    """Testing case 3: L rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(50, "a")
+    avl_tree.put(30, "b")
+    avl_tree.put(70, "c")
+    avl_tree.put(80, "c")
+    avl_tree.put(60, "d")
+    avl_tree.put(90, "e")
+    assert avl_tree.root.key == 70
+    assert avl_tree.root.balance == 0
+    assert " ".join([str(x) for x in avl_tree]) == "30 50 60 70 80 90"
+
+
+def test_R_rotation():
+    """Testing case 4: R rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(50, "a")
+    avl_tree.put(30, "b")
+    avl_tree.put(70, "c")
+    avl_tree.put(10, "c")
+    avl_tree.put(20, "d")
+    avl_tree.put(5, "e")
+    assert avl_tree.root.key == 20
+    assert avl_tree.root.balance == 0
+    assert " ".join([str(x) for x in avl_tree]) == "5 10 20 30 50 70"
+
+
+def test_RL_rotation():
+    """Testing case 4: RL rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(40, "a")
+    avl_tree.put(30, "b")
+    avl_tree.put(50, "c")
+    avl_tree.put(45, "d")
+    avl_tree.put(60, "e")
+    avl_tree.put(43, "f")
+    assert avl_tree.root.key == 45
+    assert avl_tree.root.balance == 0
+    assert avl_tree.root.child_left.key == 40
+    assert avl_tree.root.child_left.balance == 0
+    assert avl_tree.root.child_right.key == 50
+    assert avl_tree.root.child_right.balance == 1
+    assert " ".join([str(x) for x in avl_tree]) == "30 40 43 45 50 60"
+
+
+def test_LR_rotation():
+    """Testing case 6: LR rotation"""
+    avl_tree = AVLTree()
+    avl_tree.put(40, "a")
+    avl_tree.put(30, "b")
+    avl_tree.put(50, "c")
+    avl_tree.put(10, "d")
+    avl_tree.put(35, "e")
+    avl_tree.put(37, "f")
+    assert avl_tree.root.key == 35
+    assert avl_tree.root.balance == 0
+    assert avl_tree.root.child_left.key == 30
+    assert avl_tree.root.child_left.balance == -1
+    assert avl_tree.root.child_right.key == 40
+    assert avl_tree.root.child_right.balance == 0
+    assert " ".join([str(x) for x in avl_tree]) == "10 30 35 37 40 50"
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("nodes, traversal", get_cases("test_case"))
+def test_all_rotations(nodes, traversal):
+    """Testing the AVL tree rotations"""
+    avl_tree = AVLTree()
+    for k, v in nodes:
+        avl_tree.put(int(k), v)
+    assert " ".join([str(x) for x in avl_tree]) == traversal
 
 
 if __name__ == "__main__":
-    pytest.main(["test_avl_tree.py"])
+    pytest.main(["-v", "test_avl_tree.py"])
