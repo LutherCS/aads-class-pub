8befef278829cab2e92ff6bce52a5638e976691f
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-01-03 22:43:00 -0600

    Update Anomaly counter test setup

--- tests/projects/anomalycounter/test_anomalycounter.py
+++ tests/projects/anomalycounter/test_anomalycounter.py
@@ -2,7 +2,7 @@
 # encoding: UTF-8
 """
 @author: Roman Yasinovskyy
-@date: 2019
+@date: 2021
 @module: project.anomalycounter
 """
 
@@ -11,36 +11,20 @@ import pytest
 import toml
 from src.projects.anomalycounter import count
 
-THIS_DIR = pathlib.Path(".")
-TEST_DIR = pathlib.Path("tests/projects/anomalycounter/")
+
 DATA_DIR = pathlib.Path("data/projects/anomalycounter/")
-FILE_PUBLIC = pathlib.Path("test_anomalycounter_public.toml")
-FILE_SECRET = pathlib.Path("test_anomalycounter_secret.toml")
-
-if "tests" not in THIS_DIR.parts:
-    FILE_PUBLIC = THIS_DIR / TEST_DIR / FILE_PUBLIC
-    FILE_SECRET = THIS_DIR / TEST_DIR / FILE_SECRET
-
-all_test_cases = toml.load(FILE_PUBLIC)
-
 TIME_LIMIT = 1
-IMAGES = [
-    (v.get("data"), v.get("expected"))
-    for v in all_test_cases.get("case_public").get("success").values()
-]
 
-if FILE_SECRET.exists():
-    all_test_cases.update(toml.load(FILE_SECRET))
-    IMAGES.extend(
-        [
-            (v.get("data"), v.get("expected"))
-            for v in all_test_cases.get("case_secret").get("success").values()
-        ]
-    )
+
+def get_cases(category: str):
+    with open(pathlib.Path(__file__).with_suffix(".toml")) as f:
+        all_cases = toml.load(f)
+        for case in all_cases[category]:
+            yield (case.get("data"), case.get("expected"))
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("data, expected", IMAGES)
+@pytest.mark.parametrize("data, expected", get_cases("test_case"))
 def test_anomalycounter(data, expected):
     """Testing the output"""
     assert count(DATA_DIR / pathlib.Path(data)) == expected
