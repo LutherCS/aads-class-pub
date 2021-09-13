97252856d0dd8cc27dcde25679f9adbadcc0bfbb
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-09-12 21:18:11 -0500

    Make tests run from cli or gui

--- tests/exercises/hashing/test_hashing.py
+++ tests/exercises/hashing/test_hashing.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 """
-Testing the hashing functions
+`hashing` testing
+
 @authors: Roman Yasinovskyy
-@updated: 2020
+@version: 2021.9
 """
 
+import importlib
 import pathlib
+import sys
+
 import pytest
 import toml
-from src.exercises.hashing import (
-    hash_remainder,
-    hash_mid_sqr,
-    hash_folding,
-    hash_str,
-    hash_str_weighted,
-)
+
+try:
+    importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
+except ModuleNotFoundError:
+    sys.path.append(f"{pathlib.Path(__file__).parents[3]}/")
+finally:
+    from src.exercises.hashing import (
+        hash_remainder,
+        hash_mid_sqr,
+        hash_folding,
+        hash_str,
+        hash_str_weighted,
+    )
+
+TIME_LIMIT = 1
 
 
 def get_cases(category: str):
@@ -24,35 +36,40 @@ def get_cases(category: str):
             yield (case.get("keys"), case.get("size"), case.get("expected"))
 
 
+@pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("keys, size, expected", get_cases("simple_remainder"))
-def test_hash_remainder(keys, size, expected):
+def test_simple_remainder(keys, size, expected):
     """Testing simple remainder hashing"""
     assert [hash_remainder(x, size) for x in keys] == expected
 
 
+@pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("keys, size, expected", get_cases("mid_square"))
-def test_hash_hash_mid_sqr(keys, size, expected):
+def test_mid_square(keys, size, expected):
     """Testing mid-square hashing"""
     assert [hash_mid_sqr(x, size) for x in keys] == expected
 
 
+@pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("keys, size, expected", get_cases("folding"))
 def test_hash_folding(keys, size, expected):
     """Testing folding hashing"""
     assert [hash_folding(x, size) for x in keys] == expected
 
 
+@pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("keys, size, expected", get_cases("naive_string"))
-def test_hash_str(keys, size, expected):
+def test_naive_string(keys, size, expected):
     """Testing string hashing"""
     assert [hash_str(x, size) for x in keys] == expected
 
 
+@pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("keys, size, expected", get_cases("weighted_string"))
-def test_hash_str_weighted(keys, size, expected):
+def test_weighted_string(keys, size, expected):
     """Testing weighted string hashing"""
     assert [hash_str_weighted(x, size) for x in keys] == expected
 
 
 if __name__ == "__main__":
-    pytest.main(["-v", "test_hashing.py"])
+    pytest.main(["-v", __file__])
