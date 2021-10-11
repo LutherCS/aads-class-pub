6af77623c8b3e3866f6af73053582a47d48e9f9d
Roman Yasinovskyy <yasinovskyy@gmail.com>
2021-10-11 09:58:17 -0500

    Update Bloom filter exercise

--- src/exercises/bloomfilter/bloomfilter.py
+++ src/exercises/bloomfilter/bloomfilter.py
@@ -1,31 +1,56 @@
 #!/usr/bin/env python3
-"""Bloom filter implementation"""
+"""
+`bloomfilter` implementation and driver
+
+@authors:
+@version: 2021.10
+"""
 
 
 from zlib import crc32
 
 
 class BloomFilter:
-    def __init__(self, size: int = 11, k: int = 3):
+    def __init__(self, size: int = 11, k: int = 3) -> None:
         """Initialize the filter"""
-        raise NotImplementedError
+        # TODO: Implement this method
+        ...
 
-    def _hash(self, word: str) -> tuple:
+    def hash(self, word: str) -> tuple[int, ...]:
         """Return a tuple of k indices"""
-        raise NotImplementedError
+        # TODO: Implement this method
+        ...
 
-    def add(self, word: str):
+    def add(self, word: str) -> None:
         """Add a dictionary word to the filter"""
-        raise NotImplementedError
+        # TODO: Implement this method
+        ...
 
     def __contains__(self, word: str) -> bool:
         """Check if a word in in the filter"""
-        raise NotImplementedError
+        # TODO: Implement this method
+        ...
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return string representation of the filter"""
         return str(self._filter)
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Return size of the filter"""
         return len(self._filter)
+
+
+def main():
+    bf = BloomFilter()
+    dictionary = ["cat", "cow", "dog"]
+    for word in dictionary:
+        bf.add(word)
+    for word in dictionary:
+        print(f"{word:10s}{bf.hash(word)} in bf: {word in bf}")
+    dictionary2 = ["aardvark", "beaver", "cheetah", "deer", "elephant", "squirrell"]
+    for word in dictionary2:
+        print(f"{word:10s}{bf.hash(word)} in bf: {word in bf}")
+
+
+if __name__ == "__main__":
+    main()
--- tests/exercises/bloomfilter/test_bloomfilter.py
+++ tests/exercises/bloomfilter/test_bloomfilter.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """
-Testing the Bloom filter
-@author: Roman Yasinovskyy
-@date: 2020
+`bloomfilter` testing
+
+@authors: Roman Yasinovskyy
+@version: 2021.10
 """
 
+import importlib
 import pathlib
+import sys
+
 import pytest
 import toml
-from src.exercises.bloomfilter import BloomFilter
+
+try:
+    importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
+except ModuleNotFoundError:
+    sys.path.append(f"{pathlib.Path(__file__).parents[3]}/")
+finally:
+    from src.exercises.bloomfilter import BloomFilter
 
 TIME_LIMIT = 2
 
@@ -21,57 +31,13 @@ def get_cases(category: str):
 
 
 @pytest.fixture(scope="session", autouse=True)
-def bf():
+def the_bloom_filter():
     """Build Bloom filter"""
     my_filter = BloomFilter(980600, 7)
     with open("data/exercises/bloomfilter/words") as file_in:
         for word in file_in.readlines():
             my_filter.add(word.strip())
     return my_filter
-
-
-@pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("dictionary, typos, expected", get_cases("small_dictionary"))
-def test_bloomfilter_1(dictionary, typos, expected):
-    """Testing the output"""
-    bf = BloomFilter(100, 7)
-    with open(dictionary) as file_in:
-        for word in file_in.readlines():
-            bf.add(word.strip())
-    count = 0
-    with open(typos) as file_in:
-        for word in file_in.readlines():
-            if word.strip() in bf:
-                count += 1
-    assert count == expected
-
-
-@pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("dictionary, typos, expected", get_cases("small_dictionary"))
-def test_bloomfilter_2(dictionary, typos, expected):
-    """Testing the output"""
-    bf = BloomFilter(250, 7)
-    with open(dictionary) as file_in:
-        for word in file_in.readlines():
-            bf.add(word.strip())
-    count = 0
-    with open(typos) as file_in:
-        for word in file_in.readlines():
-            if word.strip() in bf:
-                count += 1
-    assert count == 0
-
-
-@pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("dictionary, typos, expected", get_cases("large_dictionary"))
-def test_bloomfilter_3(bf, dictionary, typos, expected):
-    """Testing the output"""
-    count = 0
-    with open(typos) as file_in:
-        for word in file_in.readlines():
-            if word.strip() in bf:
-                count += 1
-    assert count == expected
 
 
 @pytest.mark.timeout(TIME_LIMIT)
@@ -85,5 +51,58 @@ def test_bloomfilter_len():
     assert len(my_filter) == 980600
 
 
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("dictionary, typos, expected", get_cases("small_dictionary"))
+def test_bloomfilter_small_filter(dictionary, typos, expected):
+    """Testing the output
+
+    Since the filter size is small, some mishits are expected
+    """
+    bf = BloomFilter(100, 7)
+    with open(dictionary) as file_in:
+        for word in file_in.readlines():
+            bf.add(word.strip())
+    count = 0
+    with open(typos) as file_in:
+        for word in file_in.readlines():
+            if word.strip() in bf:
+                count += 1
+    assert count == expected
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("dictionary, typos, _", get_cases("small_dictionary"))
+def test_bloomfilter_medium_filter(dictionary, typos, _):
+    """Testing the output
+
+    Since the filter size is sufficient, mishits are not expected
+    """
+    bf = BloomFilter(250, 7)
+    with open(dictionary) as file_in:
+        for word in file_in.readlines():
+            bf.add(word.strip())
+    count = 0
+    with open(typos) as file_in:
+        for word in file_in.readlines():
+            if word.strip() in bf:
+                count += 1
+    assert count == 0
+
+
+@pytest.mark.timeout(TIME_LIMIT)
+@pytest.mark.parametrize("_, typos, expected", get_cases("large_dictionary"))
+def test_bloomfilter_large_filter(the_bloom_filter, _, typos, expected):
+    """Testing the output
+
+    Even though the filter size is large, some mishits are expected
+    """
+    count = 0
+    with open(typos) as file_in:
+        for word in file_in.readlines():
+            if word.strip() in the_bloom_filter:
+                count += 1
+    assert count == expected
+
+
 if __name__ == "__main__":
-    pytest.main(["-v", "test_bloomfilter.py"])
+    pytest.main(["-v", __file__])
