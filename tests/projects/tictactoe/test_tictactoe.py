f837df319d76258fe0d8b3ab129e88d2ceb42377
Roman Yasinovskyy <yasinovskyy@gmail.com>
2024-09-22 23:30:55 -0500

    Swith to tomllib

--- tests/projects/tictactoe/test_tictactoe.py
+++ tests/projects/tictactoe/test_tictactoe.py
@@ -3,7 +3,7 @@
 `tictactoe` testing
 
 @authors: Roman Yasinovskyy
-@version: 2021.9
+@version: 2024.9
 """
 
 import importlib
@@ -11,9 +11,10 @@ import pathlib
 import sys
 import tkinter
 from turtle import RawTurtle
+from typing import Generator
 
 import pytest
-import toml
+import tomllib
 
 try:
     importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
@@ -26,32 +27,36 @@ finally:
 TIME_LIMIT = 5
 
 
-def get_cases(category: str):
-    with open(pathlib.Path(__file__).with_suffix(".toml")) as f:
-        all_cases = toml.load(f)
+def get_cases(category: str, *attribs: str) -> Generator:
+    """Get test cases from the TOML file"""
+    with open(pathlib.Path(__file__).with_suffix(".toml"), "rb") as file:
+        all_cases = tomllib.load(file)
         for case in all_cases[category]:
-            yield (case.get("board"), case.get("evaluation"))
+            yield tuple(case.get(a) for a in attribs)
 
 
-@pytest.fixture
-def board0():
+@pytest.fixture(name="board0")
+def fixture_board0():
+    """An empty board"""
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root)
-    t = RawTurtle(canvas)
-    screen = t.getscreen()
+    raw_t = RawTurtle(canvas)
+    screen = raw_t.getscreen()
     board = Board(None, screen)
     board.reset()
     # root.destroy()
     return board
 
 
-@pytest.fixture
-def board1():
+@pytest.fixture(name="board1")
+def fixture_board1():
+    """An empty board"""
     return Board()
 
 
-@pytest.fixture
-def board2():
+@pytest.fixture(name="board2")
+def fixture_board2():
+    """Board of all 0"""
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root)
     board = Board()
@@ -62,8 +67,9 @@ def board2():
     return board
 
 
-@pytest.fixture
-def board3():
+@pytest.fixture(name="board3")
+def fixture_board3():
+    """Board of all 0"""
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root)
     board = Board()
@@ -74,8 +80,9 @@ def board3():
     return board
 
 
-@pytest.fixture
-def board4():
+@pytest.fixture(name="board4")
+def fixture_board4():
+    """Board of all X"""
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root)
     board = Board()
@@ -86,64 +93,59 @@ def board4():
     return board
 
 
-def test_board_eq_01(board0, board1):
+def test_board_eq_01(board0: Board, board1: Board):
     """Testing the board equality"""
     assert board0 == board1
 
 
-def test_board_eq_12(board1, board2):
+def test_board_eq_12(board1: Board, board2: Board):
     """Testing the board equality"""
     assert board1 != board2
 
 
-def test_board_eq_23(board2, board3):
+def test_board_eq_23(board2: Board, board3: Board):
     """Testing the board equality"""
     assert board2 == board3
 
 
-def test_board_eq_34(board3, board4):
+def test_board_eq_34(board3: Board, board4: Board):
     """Testing the board equality"""
     assert board3 != board4
 
 
-def test_board_eval_1(board1):
+def test_board_eval_1(board1: Board):
     """Testing the board valuation"""
     assert board1.eval() == 0
 
 
-def test_board_eval_2(board2, board3):
+def test_board_eval_2(board2: Board, board3: Board):
     """Testing the board valuation"""
     assert board2.eval() == -1
     assert board3.eval() == -1
 
 
-def test_board_eval_4(board4):
+def test_board_eval_4(board4: Board):
     """Testing the board valuation"""
     assert board4.eval() == 1
 
 
-def test_board_full_1(board1):
+def test_board_full_1(board1: Board):
     """Testing the board fulness"""
     assert not board1.full()
 
 
-def test_board_full_2(board2, board3):
+def test_board_full_2(board2: Board, board3: Board):
     """Testing the board fulness"""
     assert board2.full()
     assert board3.full()
 
 
-def test_board_full_4(board4):
+def test_board_full_4(board4: Board):
     """Testing the board fulness"""
-    root = tkinter.Tk()
-    canvas = tkinter.Canvas(root)
-    t = RawTurtle(canvas)
-    screen = t.getscreen()
-    board = Board(None, screen)
-    root.destroy()
+    assert board4.full()
 
 
-def test_board_avail_1(board1):
+def test_board_avail_1(board1: Board):
     """Testing the board availability"""
     assert board1.available() == [
         (0, 0),
@@ -158,61 +160,63 @@ def test_board_avail_1(board1):
     ]
 
 
-def test_board_avail_2(board2, board3):
+def test_board_avail_2(board2: Board, board3: Board):
     """Testing the board availability"""
     assert board2.available() == []
     assert board3.available() == []
 
 
-def test_board_avail_4(board4):
+def test_board_avail_4(board4: Board):
     """Testing the board availability"""
     assert board4.available() == []
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_0c(board0):
+def test_minimax_0c(board0: Board):
     """Testing the minimax"""
     assert minimax(1, board0, 3) == 0
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_0h(board0):
+def test_minimax_0h(board0: Board):
     """Testing the minimax"""
     assert minimax(-1, board0, 3) == 0
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_2c(board3):
+def test_minimax_2c(board3: Board):
     """Testing the minimax"""
     assert minimax(1, board3) == -1
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_2h(board3):
+def test_minimax_2h(board3: Board):
     """Testing the minimax"""
     assert minimax(-1, board3) == -1
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_4c(board4):
+def test_minimax_4c(board4: Board):
     """Testing the minimax"""
     assert minimax(1, board4) == 1
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_minimax_4h(board4):
+def test_minimax_4h(board4: Board):
     """Testing the minimax"""
     assert minimax(-1, board4) == 1
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-@pytest.mark.parametrize("board, evaluation", get_cases("test_board"))
-def test_minimax_param(board, evaluation):
+@pytest.mark.parametrize(
+    "board, evaluation", get_cases("test_case", "board", "evaluation")
+)
+def test_minimax_param(board: Board, evaluation: int):
     """Testing the minimax"""
     root = tkinter.Tk()
     canvas = tkinter.Canvas(root)
-    t = RawTurtle(canvas)
-    screen = t.getscreen()
+    raw_t = RawTurtle(canvas)
+    screen = raw_t.getscreen()
     tkboard = Board(None, screen)
     tkboard.reset()
     for i in range(3):
