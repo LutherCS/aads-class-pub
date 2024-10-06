cb13a16db1e28443112c1186a97c127712f4fb6b
Roman Yasinovskyy <yasinovskyy@users.noreply.github.com>
2024-10-06 16:05:24 -0500

    Update AVL trees exercise

--- tests/exercises/avl_tree/test_avl_tree.py
+++ tests/exercises/avl_tree/test_avl_tree.py
@@ -3,7 +3,7 @@
 `avl_tree` testing
 
 @authors: Roman Yasinovskyy
-@version: 2022.10
+@version: 2024.10
 """
 
 import importlib
@@ -12,7 +12,7 @@ import sys
 from typing import Generator
 
 import pytest
-import toml
+import tomllib
 
 try:
     importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
@@ -27,8 +27,8 @@ TIME_LIMIT = 1
 
 def get_cases(category: str, *attribs: str) -> Generator:
     """Get test cases from the TOML file"""
-    with open(pathlib.Path(__file__).with_suffix(".toml"), encoding="utf-8") as file:
-        all_cases = toml.load(file)
+    with open(pathlib.Path(__file__).with_suffix(".toml"), "rb") as file:
+        all_cases = tomllib.load(file)
         for case in all_cases[category]:
             yield tuple(case.get(a) for a in attribs)
 
