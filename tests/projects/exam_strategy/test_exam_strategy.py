86cb18d53030d344fec469ed5bd620c74d91bd16
Roman Yasinovskyy <yasinovskyy@users.noreply.github.com>
2024-11-14 18:56:09 -0600

    Switch to tomllib

--- tests/projects/exam_strategy/test_exam_strategy.py
+++ tests/projects/exam_strategy/test_exam_strategy.py
@@ -3,7 +3,7 @@
 `exam_strategy` testing
 
 @authors: Roman Yasinovskyy
-@version: 2021.10
+@version: 2024.11
 """
 
 import importlib
@@ -12,7 +12,7 @@ import sys
 from typing import Generator
 
 import pytest
-import toml
+import tomllib
 
 try:
     importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
@@ -28,8 +28,8 @@ TIME_LIMIT = 1
 
 def get_cases(category: str, *attribs: str) -> Generator:
     """Get test cases from the TOML file"""
-    with open(pathlib.Path(__file__).with_suffix(".toml"), encoding="utf-8") as file:
-        all_cases = toml.load(file)
+    with open(pathlib.Path(__file__).with_suffix(".toml"), "rb") as file:
+        all_cases = tomllib.load(file)
         for case in all_cases[category]:
             yield tuple(case.get(a) for a in attribs)
 
